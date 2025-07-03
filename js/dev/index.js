e23763efc95154362ebd70ead80b539dd4ba2f73
BlackMatrini <goncharvlad000@gmail.com>
2025-07-03 19:20:34 +0300

    Updates

--- js/dev/index.js
+++ js/dev/index.js
@@ -35,6 +35,11 @@
     fetch(link.href, fetchOpts);
   }
 })();
+function getHash() {
+  if (location.hash) {
+    return location.hash.replace("#", "");
+  }
+}
 let slideUp = (target, duration = 500, showmore = 0) => {
   if (!target.classList.contains("--slide")) {
     target.classList.add("--slide");
@@ -162,6 +167,39 @@ function dataMediaQueries(array, dataSetValue) {
     return { itemsArray, matchMedia };
   });
 }
+const gotoBlock = (targetBlock, noHeader = false, speed = 500, offsetTop = 0) => {
+  const targetBlockElement = document.querySelector(targetBlock);
+  if (targetBlockElement) {
+    let headerItem = "";
+    let headerItemHeight = 0;
+    if (noHeader) {
+      headerItem = "header.header";
+      const headerElement = document.querySelector(headerItem);
+      if (!headerElement.classList.contains("--header-scroll")) {
+        headerElement.style.cssText = `transition-duration: 0s;`;
+        headerElement.classList.add("--header-scroll");
+        headerItemHeight = headerElement.offsetHeight;
+        headerElement.classList.remove("--header-scroll");
+        setTimeout(() => {
+          headerElement.style.cssText = ``;
+        }, 0);
+      } else {
+        headerItemHeight = headerElement.offsetHeight;
+      }
+    }
+    if (document.documentElement.hasAttribute("data-fls-menu-open")) {
+      bodyUnlock();
+      document.documentElement.removeAttribute("data-fls-menu-open");
+    }
+    let targetBlockElementPosition = targetBlockElement.getBoundingClientRect().top + scrollY;
+    targetBlockElementPosition = headerItemHeight ? targetBlockElementPosition - headerItemHeight : targetBlockElementPosition;
+    targetBlockElementPosition = offsetTop ? targetBlockElementPosition - offsetTop : targetBlockElementPosition;
+    window.scrollTo({
+      top: targetBlockElementPosition,
+      behavior: "smooth"
+    });
+  }
+};
 function spollers() {
   const spollersArray = document.querySelectorAll("[data-fls-spollers]");
   if (spollersArray.length > 0) {
@@ -4686,6 +4724,38 @@ function menuInit() {
   });
 }
 document.querySelector("[data-fls-menu]") ? window.addEventListener("load", menuInit) : null;
+function headerScroll() {
+  const header = document.querySelector("[data-fls-header-scroll]");
+  const headerShow = header.hasAttribute("data-fls-header-scroll-show");
+  const headerShowTimer = header.dataset.flsHeaderScrollShow ? header.dataset.flsHeaderScrollShow : 500;
+  const startPoint = header.dataset.flsHeaderScroll ? header.dataset.flsHeaderScroll : 1;
+  let scrollDirection = 0;
+  let timer;
+  document.addEventListener("scroll", function(e) {
+    const scrollTop = window.scrollY;
+    clearTimeout(timer);
+    if (scrollTop >= startPoint) {
+      !header.classList.contains("--header-scroll") ? header.classList.add("--header-scroll") : null;
+      if (headerShow) {
+        if (scrollTop > scrollDirection) {
+          header.classList.contains("--header-show") ? header.classList.remove("--header-show") : null;
+        } else {
+          !header.classList.contains("--header-show") ? header.classList.add("--header-show") : null;
+        }
+        timer = setTimeout(() => {
+          !header.classList.contains("--header-show") ? header.classList.add("--header-show") : null;
+        }, headerShowTimer);
+      }
+    } else {
+      header.classList.contains("--header-scroll") ? header.classList.remove("--header-scroll") : null;
+      if (headerShow) {
+        header.classList.contains("--header-show") ? header.classList.remove("--header-show") : null;
+      }
+    }
+    scrollDirection = scrollTop <= 0 ? 0 : scrollTop;
+  });
+}
+document.querySelector("[data-fls-header-scroll]") ? window.addEventListener("load", headerScroll) : null;
 function formQuantity() {
   document.addEventListener("click", quantityActions);
   document.addEventListener("input", quantityActions);
@@ -4763,3 +4833,264 @@ function addToCart() {
   }
 }
 document.querySelector("[data-fls-addtocart]") ? window.addEventListener("load", addToCart) : null;
+function pageNavigation() {
+  document.addEventListener("click", pageNavigationAction);
+  document.addEventListener("watcherCallback", pageNavigationAction);
+  function pageNavigationAction(e) {
+    if (e.type === "click") {
+      const targetElement = e.target;
+      if (targetElement.closest("[data-fls-scrollto]")) {
+        const gotoLink = targetElement.closest("[data-fls-scrollto]");
+        const gotoLinkSelector = gotoLink.dataset.flsScrollto ? gotoLink.dataset.flsScrollto : "";
+        const noHeader = gotoLink.hasAttribute("data-fls-scrollto-header") ? true : false;
+        const gotoSpeed = gotoLink.dataset.flsScrolltoSpeed ? gotoLink.dataset.flsScrolltoSpeed : 500;
+        const offsetTop = gotoLink.dataset.flsScrolltoTop ? parseInt(gotoLink.dataset.flsScrolltoTop) : 0;
+        if (window.fullpage) {
+          const fullpageSection = document.querySelector(`${gotoLinkSelector}`).closest("[data-fls-fullpage-section]");
+          const fullpageSectionId = fullpageSection ? +fullpageSection.dataset.flsFullpageId : null;
+          if (fullpageSectionId !== null) {
+            window.fullpage.switchingSection(fullpageSectionId);
+            if (document.documentElement.hasAttribute("data-fls-menu-open")) {
+              bodyUnlock();
+              document.documentElement.removeAttribute("data-fls-menu-open");
+            }
+          }
+        } else {
+          gotoBlock(gotoLinkSelector, noHeader, gotoSpeed, offsetTop);
+        }
+        e.preventDefault();
+      }
+    } else if (e.type === "watcherCallback" && e.detail) {
+      const entry = e.detail.entry;
+      const targetElement = entry.target;
+      if (targetElement.dataset.flsWatcher === "navigator") {
+        document.querySelector(`[data-fls-scrollto].--navigator-active`);
+        let navigatorCurrentItem;
+        if (targetElement.id && document.querySelector(`[data-fls-scrollto="#${targetElement.id}"]`)) {
+          navigatorCurrentItem = document.querySelector(`[data-fls-scrollto="#${targetElement.id}"]`);
+        } else if (targetElement.classList.length) {
+          for (let index = 0; index < targetElement.classList.length; index++) {
+            const element = targetElement.classList[index];
+            if (document.querySelector(`[data-fls-scrollto=".${element}"]`)) {
+              navigatorCurrentItem = document.querySelector(`[data-fls-scrollto=".${element}"]`);
+              break;
+            }
+          }
+        }
+        if (entry.isIntersecting) {
+          navigatorCurrentItem ? navigatorCurrentItem.classList.add("--navigator-active") : null;
+        } else {
+          navigatorCurrentItem ? navigatorCurrentItem.classList.remove("--navigator-active") : null;
+        }
+      }
+    }
+  }
+  if (getHash()) {
+    let goToHash;
+    if (document.querySelector(`#${getHash()}`)) {
+      goToHash = `#${getHash()}`;
+    } else if (document.querySelector(`.${getHash()}`)) {
+      goToHash = `.${getHash()}`;
+    }
+    goToHash ? gotoBlock(goToHash) : null;
+  }
+}
+document.querySelector("[data-fls-scrollto]") ? window.addEventListener("load", pageNavigation) : null;
+const marquee = () => {
+  const $marqueeArray = document.querySelectorAll("[data-fls-marquee]");
+  const ATTR_NAMES = {
+    inner: "data-fls-marquee-inner",
+    item: "data-fls-marquee-item"
+  };
+  if (!$marqueeArray.length) return;
+  const { head } = document;
+  const debounce = (delay, fn) => {
+    let timerId;
+    return (...args) => {
+      if (timerId) {
+        clearTimeout(timerId);
+      }
+      timerId = setTimeout(() => {
+        fn(...args);
+        timerId = null;
+      }, delay);
+    };
+  };
+  const onWindowWidthResize = (cb) => {
+    if (!cb && !isFunction(cb)) return;
+    let prevWidth = 0;
+    const handleResize = () => {
+      const currentWidth = window.innerWidth;
+      if (prevWidth !== currentWidth) {
+        prevWidth = currentWidth;
+        cb();
+      }
+    };
+    window.addEventListener("resize", debounce(50, handleResize));
+    handleResize();
+  };
+  const buildMarquee = (marqueeNode) => {
+    if (!marqueeNode) return;
+    const $marquee = marqueeNode;
+    const $childElements = $marquee.children;
+    if (!$childElements.length) return;
+    Array.from($childElements).forEach(($childItem) => $childItem.setAttribute(ATTR_NAMES.item, ""));
+    const htmlStructure = `<div ${ATTR_NAMES.inner}>${$marquee.innerHTML}</div>`;
+    $marquee.innerHTML = htmlStructure;
+  };
+  const getElSize = ($el, isVertical) => {
+    if (isVertical) return $el.offsetHeight;
+    return $el.offsetWidth;
+  };
+  $marqueeArray.forEach(($wrapper) => {
+    var _a;
+    if (!$wrapper) return;
+    buildMarquee($wrapper);
+    const $marqueeInner = $wrapper.firstElementChild;
+    let cacheArray = [];
+    if (!$marqueeInner) return;
+    const dataMarqueeSpace = parseFloat($wrapper.getAttribute("data-fls-marquee-space"));
+    const $items = $wrapper.querySelectorAll(`[${ATTR_NAMES.item}]`);
+    const speed = parseFloat($wrapper.getAttribute("data-fls-marquee-speed")) / 10 || 100;
+    const isMousePaused = $wrapper.hasAttribute("data-fls-marquee-pause-mouse-enter");
+    const direction = $wrapper.getAttribute("data-fls-marquee-direction");
+    const isVertical = direction === "bottom" || direction === "top";
+    const animName = `marqueeAnimation-${Math.floor(Math.random() * 1e7)}`;
+    let spaceBetweenItem = parseFloat((_a = window.getComputedStyle($items[0])) == null ? void 0 : _a.getPropertyValue("margin-right"));
+    let spaceBetween = spaceBetweenItem ? spaceBetweenItem : !isNaN(dataMarqueeSpace) ? dataMarqueeSpace : 30;
+    let startPosition = parseFloat($wrapper.getAttribute("data-fls-marquee-start")) || 0;
+    let sumSize = 0;
+    let firstScreenVisibleSize = 0;
+    let initialSizeElements = 0;
+    let initialElementsLength = $marqueeInner.children.length;
+    let index = 0;
+    let counterDuplicateElements = 0;
+    const initEvents = () => {
+      if (startPosition) $marqueeInner.addEventListener("animationiteration", onChangeStartPosition);
+      if (!isMousePaused) return;
+      $marqueeInner.removeEventListener("mouseenter", onChangePaused);
+      $marqueeInner.removeEventListener("mouseleave", onChangePaused);
+      $marqueeInner.addEventListener("mouseenter", onChangePaused);
+      $marqueeInner.addEventListener("mouseleave", onChangePaused);
+    };
+    const onChangeStartPosition = () => {
+      startPosition = 0;
+      $marqueeInner.removeEventListener("animationiteration", onChangeStartPosition);
+      onResize2();
+    };
+    const setBaseStyles = (firstScreenVisibleSize2) => {
+      let baseStyle = "display: flex; flex-wrap: nowrap;";
+      if (isVertical) {
+        baseStyle += `
+				flex-direction: column;
+				position: relative;
+				will-change: transform;`;
+        if (direction === "bottom") {
+          baseStyle += `top: -${firstScreenVisibleSize2}px;`;
+        }
+      } else {
+        baseStyle += `
+				position: relative;
+				will-change: transform;`;
+        if (direction === "right") {
+          baseStyle += `inset-inline-start: -${firstScreenVisibleSize2}px;;`;
+        }
+      }
+      $marqueeInner.style.cssText = baseStyle;
+    };
+    const setdirectionAnim = (totalWidth) => {
+      switch (direction) {
+        case "right":
+        case "bottom":
+          return totalWidth;
+        default:
+          return -totalWidth;
+      }
+    };
+    const animation = () => {
+      const keyFrameCss = `@keyframes ${animName} {
+					 0% {
+						 transform: translate${isVertical ? "Y" : "X"}(${!isVertical && window.stateRtl ? -startPosition : startPosition}%);
+					 }
+					 100% {
+						 transform: translate${isVertical ? "Y" : "X"}(${setdirectionAnim(
+        !isVertical && window.stateRtl ? -firstScreenVisibleSize : firstScreenVisibleSize
+      )}px);
+					 }
+				 }`;
+      const $style = document.createElement("style");
+      $style.classList.add(animName);
+      $style.innerHTML = keyFrameCss;
+      head.append($style);
+      $marqueeInner.style.animation = `${animName} ${(firstScreenVisibleSize + startPosition * firstScreenVisibleSize / 100) / speed}s infinite linear`;
+    };
+    const addDublicateElements = () => {
+      sumSize = firstScreenVisibleSize = initialSizeElements = counterDuplicateElements = index = 0;
+      const $parentNodeWidth = getElSize($wrapper, isVertical);
+      let $childrenEl = Array.from($marqueeInner.children);
+      if (!$childrenEl.length) return;
+      if (!cacheArray.length) {
+        cacheArray = $childrenEl.map(($item) => $item);
+      } else {
+        $childrenEl = [...cacheArray];
+      }
+      $marqueeInner.style.display = "flex";
+      if (isVertical) $marqueeInner.style.flexDirection = "column";
+      $marqueeInner.innerHTML = "";
+      $childrenEl.forEach(($item) => {
+        $marqueeInner.append($item);
+      });
+      $childrenEl.forEach(($item) => {
+        if (isVertical) {
+          $item.style.marginBottom = `${spaceBetween}px`;
+        } else {
+          $item.style.marginRight = `${spaceBetween}px`;
+          $item.style.flexShrink = 0;
+        }
+        const sizeEl = getElSize($item, isVertical);
+        sumSize += sizeEl + spaceBetween;
+        firstScreenVisibleSize += sizeEl + spaceBetween;
+        initialSizeElements += sizeEl + spaceBetween;
+        counterDuplicateElements += 1;
+        return sizeEl;
+      });
+      const $multiplyWidth = $parentNodeWidth * 2 + initialSizeElements;
+      for (; sumSize < $multiplyWidth; index += 1) {
+        if (!$childrenEl[index]) index = 0;
+        const $cloneNone = $childrenEl[index].cloneNode(true);
+        const $lastElement = $marqueeInner.children[index];
+        $marqueeInner.append($cloneNone);
+        sumSize += getElSize($lastElement, isVertical) + spaceBetween;
+        if (firstScreenVisibleSize < $parentNodeWidth || counterDuplicateElements % initialElementsLength !== 0) {
+          counterDuplicateElements += 1;
+          firstScreenVisibleSize += getElSize($lastElement, isVertical) + spaceBetween;
+        }
+      }
+      setBaseStyles(firstScreenVisibleSize);
+    };
+    const correctSpaceBetween = () => {
+      if (spaceBetweenItem) {
+        $items.forEach(($item) => $item.style.removeProperty("margin-right"));
+        spaceBetweenItem = parseFloat(window.getComputedStyle($items[0]).getPropertyValue("margin-right"));
+        spaceBetween = spaceBetweenItem ? spaceBetweenItem : !isNaN(dataMarqueeSpace) ? dataMarqueeSpace : 30;
+      }
+    };
+    const init = () => {
+      correctSpaceBetween();
+      addDublicateElements();
+      animation();
+      initEvents();
+    };
+    const onResize2 = () => {
+      var _a2;
+      (_a2 = head.querySelector(`.${animName}`)) == null ? void 0 : _a2.remove();
+      init();
+    };
+    const onChangePaused = (e) => {
+      const { type, target } = e;
+      target.style.animationPlayState = type === "mouseenter" ? "paused" : "running";
+    };
+    onWindowWidthResize(onResize2);
+  });
+};
+marquee();
